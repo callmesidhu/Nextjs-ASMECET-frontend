4819d3eda3533d901e90ca43abf159f2b650e192
S Sidharth <sidharthpunalur@gmail.com>
2024-09-18 18:51:13 +0530

    Folder stucture added

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function page() {
+function About() {
   return (
     <div>
-      <h1>ASME-CET</h1>
+      
     </div>
   )
 }
 
-export default page
+export default About
